a86d45dd39f80723dea43775268c2cf327ac7a9b
orlandosoft <orlandodaw@gmail.com>
2016-07-25 14:25:33 -0500

    playgroundPractica

---
+++ Desktop/prueba.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+
+import UIKit
+
+
+var str = "Hello, playground"
+str = "loquequieras"
+var letras="pralfjaofj"
+var coleccion=["prueba","pruebas","probando"]
+let numerosColeccion=[1,2,4,53,43]
+let diccionario=["clave":"hola","clave2":"prueba","clave3":"probando"]
+let letras2=String("hola")
+let entero=Int(2323)
+var falsoVerdadero=Bool(true)
+var boton=UIButton()
+var etiqueta=UILabel()
+
+
+let mystring:String
+mystring="String"
